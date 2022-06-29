820aa784b4b5ac7b490831a2cfd1ebc3dc4751a5
Asqarbek Olimov <88034288+AsqarbekOlimov@users.noreply.github.com>
2022-06-29 08:53:30 +0500

    Json-server

--- js/script.js
+++ js/script.js
@@ -311,4 +311,8 @@ window.addEventListener('DOMContentLoaded', () => {
       closeModal()
     }, 4000)
   }
+
+  fetch('db.json')
+    .then((data) => data.json())
+    .then((res) => console.log(res))
 })
